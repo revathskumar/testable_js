0b4a940d9cea9e84c81944b8bb213f407fc2480c
Revath S Kumar <rsk@revathskumar.com>
2013-06-01 18:19:49 +0530

    moved tests into suites

--- test/spec/likes_test.js
+++ test/spec/likes_test.js
@@ -1,23 +1,24 @@
 /*global setup, test, $, Likes, assert */
 'use strict';
 
-var ul;
+suite('Likes', function(){
+    var ul;
 
-setup(function(){
-    ul = $('<ul><li class="no-results"></li></ul>');
+    setup(function(){
+        ul = $('<ul><li class="no-results"></li></ul>');
+    });
+
+    test('constructor', function(){
+        var l = new Likes(ul);
+        assert(l);
+    });
+
+    test('adding name', function(){
+        var l = new Likes(ul);
+        l.add('Addy');
+
+        assert.equal(ul.find('li').length, 1);
+        assert.equal(ul.find('li').first().html(), 'Addy');
+        assert.equal(ul.find('li.no-results').length, 0);
+    });
 });
-
-test('constructor', function(){
-    var l = new Likes(ul);
-    assert(l);
-});
-
-test('adding name', function(){
-    var l = new Likes(ul);
-    l.add('Addy');
-
-    assert.equal(ul.find('li').length, 1);
-    assert.equal(ul.find('li').first().html(), 'Addy');
-    assert.equal(ul.find('li.no-results').length, 0);
-});
-
--- test/spec/search_results_test.js
+++ test/spec/search_results_test.js
@@ -1,14 +1,26 @@
 /* global setup, test, $, SearchResults, assert */
 'use strict';
 
-var ul;
+suite('Search Results', function(){
+   var ul;
+    var data = {users:[{name: "Addy"}, {name: "Paul"}]};
 
-setup(function(){
-    ul = $('<ul><li class="no-results">No results</li></ul>');
-});
+    setup(function(){
+        ul = $('<ul><li class="no-results">No results</li></ul>');
+    });
 
-test('constructor', function(){
-    var s = new SearchResults(ul);
-    assert(s);
-});
+    test('constructor', function(){
+        var s = new SearchResults(ul);
+        assert(s);
+    });
+
+    test('display results', function(){
+        var s = new SearchResults(ul);
+        s.setResults(data);
+
+        assert.equal(ul.find('li').length, data.users.length);
+        assert.equal(ul.find('li').first().html(), data.users[0].name);
+        assert.equal(ul.find('li.no-results').length, 0);
+    });
+})
 
